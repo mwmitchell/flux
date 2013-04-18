b486c138ffef67deac78023c194723e72e27b59b
Matt Mitchell <goodieboy@gmail.com>
2013-04-18 13:42:42 -0400

    excluding org.apache.lucene/lucene-analyzers-morfologik - duplicate entry error when buliding

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.solr/solr-core "4.2.1"]
+                 [org.apache.solr/solr-core "4.2.1"
+                   :exclusions [org.apache.lucene/lucene-analyzers-morfologik]]
                  [org.apache.solr/solr-solrj "4.2.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
