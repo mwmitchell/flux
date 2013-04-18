79c2c7623611f6a6bb4a2f6e6d2420f42ff3beb4
Matt Mitchell <goodieboy@gmail.com>
2013-04-18 13:37:17 -0400

    added project group

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject flux "0.1.0"
+(defproject codesignals.flux "0.1.0"
   :description "A clojure client library for Solr"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.solr/solr-core "4.2.0"]
-                 [org.apache.solr/solr-solrj "4.2.0"]]
+                 [org.apache.solr/solr-core "4.2.1"]
+                 [org.apache.solr/solr-solrj "4.2.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
