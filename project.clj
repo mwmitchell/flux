f6013347e008d1674e2424fe6396582856147c81
Matt Mitchell <goodieboy@gmail.com>
2013-09-26 20:31:16 -0400

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codesignals/flux "0.1.1"
+(defproject com.codesignals/flux "0.2.0"
   :description "A clojure client library for Solr"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
