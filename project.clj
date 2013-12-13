30fdac325c1ee212f013ffe05bf8591de8bf6a6f
Matt Mitchell <mmitchell@roomkey.com>
2013-12-13 14:18:32 -0500

    Bumped Solr to 4.6.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.solr/solr-core "4.4.0"]
-                 [org.apache.solr/solr-solrj "4.4.0"]]
+                 [org.apache.solr/solr-core "4.6.0"]
+                 [org.apache.solr/solr-solrj "4.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
