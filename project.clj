8650ec10d54d36d5018c92f03d786c2632db77f5
Matt Mitchell <goodieboy@gmail.com>
2014-08-20 20:50:25 -0400

    bumped solr j and core to 4.9.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.solr/solr-core "4.6.0"]
-                 [org.apache.solr/solr-solrj "4.6.0"]
+                 [org.apache.solr/solr-core "4.9.0"]
+                 [org.apache.solr/solr-solrj "4.9.0"]
                  [javax.servlet/servlet-api "2.5"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
