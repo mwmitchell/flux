027f9d2a87427118cf2f8ecb397b6ad45f96a028
Matt Mitchell <goodieboy@gmail.com>
2014-08-20 20:20:38 -0400

    Bumped Clojure to 1.6.0
    * https://github.com/mwmitchell/flux/issues/1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.solr/solr-core "4.6.0"]
                  [org.apache.solr/solr-solrj "4.6.0"]
                  [javax.servlet/servlet-api "2.5"]]
