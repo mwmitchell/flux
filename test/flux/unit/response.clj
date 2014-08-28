mwmitchell/flux

commit 62ae93eb662b20b16f029780bf13a5b71dfcfca6

flux.response now using defmulti for conversions

src/flux/response.clj
@@ -1,62 +1,53 @@
 (ns flux.response
-  (:import [org.apache.solr.client.solrj.response SolrResponseBase]
+  (:import [org.apache.solr.client.solrj SolrResponse]
            [org.apache.solr.common.util NamedList SimpleOrderedMap]
-           [org.apache.solr.common SolrDocumentList]
+           [org.apache.solr.common SolrDocumentList SolrDocument]
+           [org.apache.solr.common SolrInputDocument]
            [java.util ArrayList]))
 
-(defn create-map-from-document [document]
-  (into {}
-        (for [fld (.getFieldNames document)]
-          {(keyword fld) (.getFieldValue document fld)})))
+;; TODO: Rename this ns to something like "conversion", not "response"
 
-(declare convert-value)
+(defmulti ->clojure class)
 
-(defn convert-key
-  [k]
-  (if (string? k)
-    (keyword k)
-    k))
+(defmethod ->clojure SimpleOrderedMap [obj]
+  (reduce
+   (fn [acc o]
+     (assoc acc (keyword (.getKey o)) (->clojure (.getValue o))))
+   {}
+   (iterator-seq (.iterator obj))))
 
-(defn remap-response
-  [l]
-  {:maxScore (.getMaxScore l)
-   :numFound (.getNumFound l)
-   :start (.getStart l)
-   :docs (map create-map-from-document (iterator-seq (.iterator l)))})
+(defmethod ->clojure NamedList [obj]
+  (mapv
+   #(vector (.getKey %) (->clojure (.getValue %)))
+   (iterator-seq (.iterator obj))))
 
-(defn convert-map-entry
-  "Converts a MapEntry from a NamedList into a vector with 2 values. Nested NamedLists are recursively converted. "
-  [map-entry]
-  (let [k (convert-key (.getKey map-entry))
-        v (convert-value (.getValue map-entry))]
-    [k v]))
+(defmethod ->clojure ArrayList [obj]
+  (mapv ->clojure obj))
 
-(defn- uniquify-paired-seq
-  "removes values when dup-key-in-paired-seq? returns true"
-  [coll]
-  (map #(first (val %1)) (group-by first coll)))
+(defmethod ->clojure SolrDocumentList [obj]
+  (merge
+   {:numFound (.getNumFound obj)
+    :start (.getStart obj)
+    :docs (map ->clojure (iterator-seq (.iterator obj)))}
+   (when-let [ms (.getMaxScore obj)]
+     {:maxScore ms})))
 
-(defn convert-named-list
-  "Converts a NamedList into a array-map. Nested NamedLists are recursively converted. "
-  [named-list]
-  (let [itrseq (iterator-seq (.iterator named-list))
-        converted (map convert-map-entry itrseq)
-        reduced (uniquify-paired-seq converted)
-        flattened (apply concat reduced)
-        result (apply array-map flattened)]
-    result))
+(defmethod ->clojure SolrDocument [obj]
+  (reduce
+   (fn [acc f]
+     (assoc acc (keyword f) (->clojure (.getFieldValue obj f))))
+   {}
+   (.getFieldNames obj)))
 
-(defn convert-value
-  [v]
-  (cond
-   (instance? SimpleOrderedMap v) (convert-named-list v)
-   (instance? SolrDocumentList v) (remap-response v)
-   (instance? NamedList v) (convert-named-list v)
-   (instance? ArrayList v) (vec (map convert-value v))
-   :else v))
+(defmethod ->clojure SolrResponse [obj]
+  (->clojure (.getResponse obj)))
 
-(defn response-base
-  [^SolrResponseBase r]
-  (assoc-in
-   (convert-named-list (.getResponse r))
-   [:responseHeader :elaspsedTime] (.getElapsedTime r)))
+(defmethod ->clojure SolrInputDocument [obj]
+  (reduce
+   (fn [acc o]
+     (assoc acc (keyword o) (.getFieldValue obj o)))
+   {}
+   (.getFieldNames obj)))
+
+(defmethod ->clojure :default [obj]
+  obj)

test/flux/unit/response.clj
@@ -6,4 +6,4 @@
 
 (fact "create-map-from-document"
   (let [doc (update/create-doc {:id 1})]
-    (create-map-from-document doc) => {:id 1}))
+    (->clojure doc) => {:id 1}))

src/flux/client.clj
@@ -1,10 +1,10 @@
 (ns flux.client
   (require [flux.update :refer [create-doc]]
            [flux.query :refer [create-query]]
-           [flux.response :refer [response-base]]))
+           [flux.response :refer [->clojure]]))
 
 (defn query [solr-server query & [options]]
-  (response-base (.query solr-server (create-query query options))))
+  (->clojure (.query solr-server (create-query query options))))
 
 (defmulti add
   (fn [_ input & _]
@@ -13,37 +13,37 @@
      :else :default)))
 
 (defmethod add :one [client doc & {:as opts}]
-  (response-base (.add client (create-doc doc))))
+  (->clojure (.add client (create-doc doc))))
 
 (defmethod add :default [client docs & {:as opts}]
-  (response-base (.add client (map create-doc docs))))
+  (->clojure (.add client (map create-doc docs))))
 
 (defn commit [client & {:as opts}]
-  (response-base (.commit client)))
+  (->clojure (.commit client)))
 
 (letfn [(v [x]
           (cond (keyword? x) (name x) :else (str x)))]
   (defn delete-by-id [client ids & {:as opts}]
-    (response-base
+    (->clojure
      (.deleteById client (if (coll? ids) (map v ids) (v ids))))))
 
 (defn delete-by-query [client q & {:as opts}]
-  (response-base (.deleteByQuery client q)))
+  (->clojure (.deleteByQuery client q)))
 
 (defn optimize
   ([client]
-     (response-base (.optimize client)))
+     (->clojure (.optimize client)))
   ([client wait-flush wait-searcher]
-     (response-base (.optimize client wait-flush wait-searcher)))
+     (->clojure (.optimize client wait-flush wait-searcher)))
   ([client wait-flush wait-searcher max-segments]
-     (response-base
+     (->clojure
       (.optimize client wait-flush wait-searcher max-segments))))
 
 (defn rollback [client]
-  (response-base (.rollback client)))
+  (->clojure (.rollback client)))
 
 (defn shutdown [client]
-  (.shutdown client))
+  (->clojure (.shutdown client)))
 
 (defn ping [client]
-  (.ping client))
+  (->clojure (.ping client)))

commit e28129525a05c24453ccc91a7a6440907c574d0a

Fix for NamedList conversion regression
- Commit 62ae93e introduced a regression when converting NamedList
  objects
- In previous version, covert-named-list returned a map with string
  keys converted to keywords
- The new ->clojure multi method will return a vector of 2-element
  vectors when called on a NamedList (string keys not converted)
- Unit test provided to illustrate issue

src/flux/response.clj
@@ -7,6 +7,12 @@
 
 ;; TODO: Rename this ns to something like "conversion", not "response"
 
+(defn convert-key
+  [k]
+  (if (string? k)
+    (keyword k)
+    k))
+
 (defmulti ->clojure class)
 
 (defmethod ->clojure SimpleOrderedMap [^SimpleOrderedMap obj]
@@ -17,9 +23,10 @@
    (iterator-seq (.iterator obj))))
 
 (defmethod ->clojure NamedList [^NamedList obj]
-  (mapv
-   (fn [^java.util.Map$Entry o] (vector (.getKey o) (->clojure (.getValue o))))
-   (iterator-seq (.iterator obj))))
+  (into {}
+        (mapv
+         (fn [^java.util.Map$Entry o] (vector (convert-key (.getKey o)) (->clojure (.getValue o))))
+         (iterator-seq (.iterator obj)))))
 
 (defmethod ->clojure ArrayList [obj]
   (mapv ->clojure obj))

test/flux/unit/response.clj
@@ -1,4 +1,5 @@
 (ns flux.unit.response
+  (:import [org.apache.solr.common.util NamedList])
   (:require [flux.update :as update]
             [flux.response :refer :all]
             [midje.sweet :refer :all]))
@@ -6,3 +7,7 @@
 (fact "create-map-from-document"
   (let [doc (update/create-doc {:id 1})]
     (->clojure doc) => {:id 1}))
+
+(fact "Convert named list"
+  (let [nl (NamedList. (into-array {"a" 1 "b" 2}))]
+    (->clojure nl) => {:a 1 :b 2}))