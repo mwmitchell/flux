42546a32df3555a1de249a49b37b8f653395102d
Matt Mitchell <matt.mitchell@lucidworks.com>
2016-01-18 17:38:53 -0500

    localized a few helper fns, removed :static metadata

--- src/flux/collections.clj
+++ src/flux/collections.clj
@@ -2,11 +2,6 @@
   (:require [flux.client :as client]
             [flux.query :as q]))
 
-(defn solr-node-name
-  "Returns the solr node name for given host/port"
-  [host-port]
-  (str host-port "_solr"))
-
 (defn all-replicas
   [connection collection]
   (let [request (q/create-query-request :get "/admin/collections"
@@ -15,63 +10,62 @@
         shards (get-in response [:cluster :collections (keyword collection) "shards"])]
     (into {} (map (fn [[_ v]] (get v "replicas")) shards))))
 
-(defn try-all-replicas [connection collection]
-  (try
-    (all-replicas connection collection)
-    (catch Throwable _ {})))
-
 (defn active?
   "Returns true if replica is in active state"
-  {:static true}
   [[_ {:strs [state]}]]
   (= "active" state))
 
 (defn recovering?
   "Returns true if replica is in recovery state"
-  {:static true}
   [[_ {:strs [state]}]]
   (= "recovery" state))
 
 (defn down?
   "Returns true if replica is in down state"
-  {:static true}
   [[_ {:strs [state]}]]
   (= "down" state))
 
 (defn not-active?
   "Returns true if replica is not in active state"
-  {:static true}
   [replica]
   (not (active? replica)))
 
 (defn node-name
   "Returns the node name of the replica"
-  {:static true}
   [[_ {:strs [node_name]}]]
   node_name)
 
-(defn hosted-by?
-  "Returns true if given replica is hosted locally by given host/port"
-  {:static true}
-  [replica host-port]
-  (= (node-name replica) (solr-node-name host-port)))
+(letfn [ ;; Returns the solr node name for given host/port
+        (solr-node-name [host-port]
+          (str host-port "_solr"))]
+  
+  (defn hosted-by?
+    "Returns true if given replica is hosted locally by given host/port"
+    [replica host-port]
+    (= (node-name replica) (solr-node-name host-port))))
 
 (defn leader?
   "Returns true if given replica is a leader"
-  {:static true}
   [[_ {:keys [leader]}]]
   (= "true" leader))
 
-(defn wait-until-active
-  "Waits (infinitely) until all solr replicas hosted by given host/port belonging to given collection are in active state"
-  [connection collection host-port]
-  (loop [replicas (try-all-replicas connection collection)]
-    (when-not (->> replicas
-                   (filter active?)
-                   (filter #(hosted-by? %1 host-port))
-                   (seq))
-      (Thread/sleep 1000)
-      (recur (try-all-replicas connection collection)))))
+(letfn [(try-all-replicas [connection collection]
+          (try
+            (all-replicas connection collection)
+            ;; TODO: catch specific exc.. not throwable
+            (catch Throwable _ {})))]
+  
+  (defn wait-until-active
+    "Waits (infinitely) until all solr replicas hosted by given host/port belonging to given collection are in active state"
+    [connection collection host-port]
+    (loop [replicas (try-all-replicas connection collection)]
+      (when-not (->> replicas
+                     (filter active?)
+                     (filter #(hosted-by? %1 host-port))
+                     (seq))
+        ;; TODO: allow optional timeout val
+        (Thread/sleep 1000)
+        (recur (try-all-replicas connection collection))))))
 
 (defn create-collection
   "Create a SolrCloud collection"
