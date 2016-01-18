0f2f281f268cfe4612e7c470714a749247cedbf6
Matt Mitchell <matt.mitchell@lucidworks.com>
2016-01-18 17:39:35 -0500

    Embedded converts strings to Path objects

--- src/flux/embedded.clj
+++ src/flux/embedded.clj
@@ -1,7 +1,12 @@
 (ns flux.embedded
   (import [java.io File]
           [org.apache.solr.client.solrj.embedded EmbeddedSolrServer]
-          [org.apache.solr.core CoreContainer]))
+          [org.apache.solr.core CoreContainer]
+          [java.nio.file Paths]
+          [java.net URI]))
+
+(defn- str->path [str-path]
+  (-> str-path File. .toURI Paths/get))
 
 (defn create-core-container
   "Creates a CoreContainer from a solr-home path and solr-config.xml path
@@ -14,9 +19,11 @@
    Note: If using core.properties only, it is required to call (.load core-container)
          before creating the EmbeddedSolrServer instance."
   ([^String solr-home]
-     (CoreContainer. solr-home))
+   (CoreContainer. solr-home))
   ([^String solr-home-path ^String solr-config-path]
-     (CoreContainer/createAndLoad solr-home-path (File. solr-config-path))))
+   (CoreContainer/createAndLoad
+    (str->path solr-home-path)
+    (str->path solr-config-path))))
 
 (defn create [^CoreContainer core-container core-name]
   {:pre [(some #(% core-name) [string? keyword?])]}
