da0cc097d11b9ba25503d35ea3da111e0f76a3c7
Alberto Gori <albertogori80@gmail.com>
2016-06-14 21:02:24 +0100

    add support for hierarcical document.

--- test/flux/unit/update.clj
+++ test/flux/unit/update.clj
@@ -1,6 +1,18 @@
 (ns flux.unit.update
   (:require [flux.update :refer :all]
-            [midje.sweet :refer :all]))
+            [midje.sweet :refer :all])
+  (:import [org.apache.solr.common SolrInputDocument]))
 
 (fact "create-doc"
   (class (create-doc {:id 1})) => org.apache.solr.common.SolrInputDocument)
+
+(fact "create-doc-with-children"
+  (let [doc { :id 1
+              :_childDocuments_
+              [{ :id "1.1"
+                 :title "a title..."
+                 :_childDocuments_
+                 [{:id "1.1.1"}]}]}
+        solr-doc (create-doc doc)]
+    (.hasChildDocuments solr-doc) => true
+    (-> solr-doc (.getChildDocuments) first (.hasChildDocuments)) => true))
